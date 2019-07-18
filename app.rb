abec1a27c0d87fd4494449861d59879545872026
Student <student@Makerss-MacBook-Pro.local>
2019-07-18 16:29:31 +0100

    step 12: update, complete

--- app.rb
+++ app.rb
@@ -25,5 +25,15 @@ class BookmarkManager < Sinatra::Base
     redirect('/')
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edit"
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect('/')
+  end
+
   run! if app_file == $0
 end
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -34,6 +34,15 @@ class Bookmark
     self.connection.exec("DELETE FROM bookmarks WHERE id = #{id};")
   end
 
+  def self.update(id:, title:, url:)
+    result = self.connection.exec("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+
+  def self.find(id:)
+    result = self.connection.exec("SELECT * FROM bookmarks WHERE id = #{id};")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
 
   ## non-refactored version below
 
